104da7c011b051ded65c21d8e4df7f3035a9b3e7
stardesign77 <stardesign041@gmail.com>
2018-04-11 20:09:12 +0530

    Update js-buy-sdk.js

--- node-js-buy/src/js-buy-sdk.js
+++ node-js-buy/src/js-buy-sdk.js
@@ -4,8 +4,8 @@ import Client, {Config} from 'shopify-buy';
 global.fetch = fetch;
 
 const config = new Config({
-  storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
-  domain: 'graphql.myshopify.com'
+  storefrontAccessToken: '2ad50f7ec5ac1cf199b8c7b775022e81',
+  domain: 'my-first-st.myshopify.com'
 });
 
 export default new Client(config);
